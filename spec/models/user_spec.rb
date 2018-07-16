e0796f030164cd6a8cbc2b9f4333000f2905bbdd
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-16 17:58:46 +0200

    [Add]User model via devise
    Added first name and last name to user attributes to fit product owner's request. Cover those parameters with test's

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'attributes' do
+    it 'should have proper attributes' do
+      expect(subject.attributes).to include('first_name', 'last_name')
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+
+end
