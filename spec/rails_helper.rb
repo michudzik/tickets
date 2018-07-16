f5ab817d37171bd5529c5ef0052154acb02d3497
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-16 18:30:27 +0200

    [Config]Rails-Controller-Testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # rails-controller-testing config
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
 end
 
 Shoulda::Matchers.configure do |config|
