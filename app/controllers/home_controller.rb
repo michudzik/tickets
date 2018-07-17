283ff1291255e446bf67949c51909361504c3556
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-17 12:53:59 +0200

    [Modify]Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,7 @@ class HomeController < ApplicationController
 
   def home
     if current_user
-      unless current_user.confirmed_at.nil?
-        redirect_to new_user_confirmation_url
-      else
-        redirect_to user_url(current_user.id)
-      end
+      redirect_to user_url(current_user.id)
     else
       redirect_to new_user_session_url
     end
