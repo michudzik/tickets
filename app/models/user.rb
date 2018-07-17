05fc46ac8aea01aa2ec6cc837cdd7dd093f64aa9
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-17 17:55:48 +0200

    [Add]Roles for models
    Callback to automatically assign user to a role is used in role model. Also helper methods were defined to check if user is an admin or support. Models were tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,30 @@ class User < ApplicationRecord
 
   validates :first_name,  presence: true
   validates :last_name,   presence: true
+  belongs_to :role
+
+  before_validation :default_role, on: :create
+
+  def admin?
+    self.role.name == 'admin'
+  end
+
+  def it_support?
+    self.role.name == 'it_support'
+  end
+
+  def om_support?
+    self.role.name == 'om_support'
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+
+    def default_role
+      self.role ||= Role.find_by(name: 'none')
+    end
+
 end
