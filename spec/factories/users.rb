b8d2c989de1a926275737e145e29682ff3ac23c0
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-17 21:01:32 +0200

    [Refactor]User test

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    first_name    { Faker::Name.first_name }
+    last_name     { Faker::Name.last_name }
+    email         { "#{Faker::Name.first_name}@example.com" }
+    password      'secret'
+    confirmed_at  { DateTime.now }
+    association :role, factory: :none
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,40 +3,38 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'attributes' do
-    let!(:role) { Role.create(name: 'none') }
     it 'should have proper attributes' do
       expect(subject.attributes).to include('first_name', 'last_name')
     end
   end
 
   describe 'validations' do
-    let!(:role) { Role.create(name: 'none') }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
-	it { should have_many(:comments) }
   end
 
   describe 'relations' do
-    let!(:role) { Role.create(name: 'none') }
     it { should belong_to(:role) }
+    it { should have_many(:comments) }
   end
 
   describe 'callbacks' do
-    let!(:role) { Role.create(name: 'none') }
-    let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+    let(:user) { create(:user) }
     it 'should set role to none if not given' do
       expect(user.role.name).to eq('none')
     end
   end
 
   describe 'methods' do
-    describe '#admin?' do
-      let!(:role) { Role.create(name: 'admin') }
-      let!(:role_1) { Role.create(name: 'none') }
-      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+    let!(:admin_role)        { create(:admin) }
+    let!(:none_role)         { create(:none) }
+    let!(:it_support_role)   { create(:it_support) }
+    let!(:om_support_role)   { create(:om_support) }
+    let(:user) { create(:user) }
 
+    describe '#admin?' do
       it 'should return true' do
-        user.role = role
+        user.role = admin_role
         expect(user.admin?).to eq(true)
       end
 
@@ -46,12 +44,8 @@ RSpec.describe User, type: :model do
     end
 
     describe '#it_support?' do
-      let!(:role) { Role.create(name: 'it_support') }
-      let!(:role_1) { Role.create(name: 'none') }
-      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
-
       it 'should return true' do
-        user.role = role
+        user.role = it_support_role
         expect(user.it_support?).to eq(true)
       end
 
@@ -61,12 +55,8 @@ RSpec.describe User, type: :model do
     end
 
     describe '#om_support?' do
-      let!(:role) { Role.create(name: 'om_support') }
-      let!(:role_1) { Role.create(name: 'none') }
-      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
-
       it 'should return true' do
-        user.role = role
+        user.role = om_support_role
         expect(user.om_support?).to eq(true)
       end
 
@@ -76,11 +66,8 @@ RSpec.describe User, type: :model do
     end
 
     describe '#full_name' do
-      let!(:role) { Role.create(name: 'none') }
-      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
-
       it 'should return full name' do
-        expect(user.full_name).to eq('Example Example')
+        expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
       end
     end
   end
