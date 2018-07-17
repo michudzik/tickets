c40727acb245049d0786440dee9470daceeaceca
Piotr <piotr.hacas@gmail.com>
2018-07-17 11:16:11 +0200

    [add] user_id to role table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_090004) do
+ActiveRecord::Schema.define(version: 2018_07_17_091429) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_090004) do
     t.string "role", default: "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
