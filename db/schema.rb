68e46b023b9b90fd0b9a04a83e7a6b7b70c31eb0
Piotr <piotr.hacas@gmail.com>
2018-07-17 11:23:26 +0200

    [add] migration for status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_091429) do
+ActiveRecord::Schema.define(version: 2018_07_17_092007) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2018_07_17_091429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
