d227bcbcc07d99fc98c60a816b019c3e1de55838
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-17 17:59:17 +0200

    [Add]Add test's to user model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe UsersController, type: :controller do
   
   describe '#show' do
     describe 'successful response' do
-      let!(:user) { User.create(first_name: 'example', last_name: 'example', email: 'example@example.com', password: 'secret') }
+      let!(:role) { Role.create(name: 'none') }
+      let!(:user) { User.create(first_name: 'example', last_name: 'example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
       before do
         sign_in user
+        user.confirmed_at = DateTime.now
+        user.save
+        user.reload
         get :show, params: { id: user.id } 
       end
       it { expect(response).to be_successful }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,85 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'attributes' do
+    let!(:role) { Role.create(name: 'none') }
     it 'should have proper attributes' do
       expect(subject.attributes).to include('first_name', 'last_name')
     end
   end
 
   describe 'validations' do
+    let!(:role) { Role.create(name: 'none') }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+  end
+
+  describe 'relations' do
+    let!(:role) { Role.create(name: 'none') }
+    it { should belong_to(:role) }
+  end
+
+  describe 'callbacks' do
+    let!(:role) { Role.create(name: 'none') }
+    let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+    it 'should set role to none if not given' do
+      expect(user.role.name).to eq('none')
+    end
+  end
+
+  describe 'methods' do
+    describe '#admin?' do
+      let!(:role) { Role.create(name: 'admin') }
+      let!(:role_1) { Role.create(name: 'none') }
+      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+
+      it 'should return true' do
+        user.role = role
+        expect(user.admin?).to eq(true)
+      end
+
+      it 'should return false' do
+        expect(user.admin?).to eq(false)
+      end
+    end
+
+    describe '#it_support?' do
+      let!(:role) { Role.create(name: 'it_support') }
+      let!(:role_1) { Role.create(name: 'none') }
+      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+
+      it 'should return true' do
+        user.role = role
+        expect(user.it_support?).to eq(true)
+      end
+
+      it 'should return false' do
+        expect(user.it_support?).to eq(false)
+      end
+    end
+
+    describe '#om_support?' do
+      let!(:role) { Role.create(name: 'om_support') }
+      let!(:role_1) { Role.create(name: 'none') }
+      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+
+      it 'should return true' do
+        user.role = role
+        expect(user.om_support?).to eq(true)
+      end
+
+      it 'should return false' do
+        expect(user.om_support?).to eq(false)
+      end
+    end
+
+    describe '#full_name' do
+      let!(:role) { Role.create(name: 'none') }
+      let(:user) { User.create(first_name: 'Example', last_name: 'Example', email: "#{Faker::Name.first_name}@example.com", password: 'secret') }
+
+      it 'should return full name' do
+        expect(user.full_name).to eq('Example Example')
+      end
+    end
   end
 
 end
