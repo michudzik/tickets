5d6749af70d3de8f614f607042c7bbb675315d3e
Piotr <piotr.hacas@gmail.com>
2018-07-17 13:00:25 +0200

    [preprare] for push

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+	it { should have_many(:comments) }
   end
 
 end
