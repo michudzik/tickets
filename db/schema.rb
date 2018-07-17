71cf1162c13535ae822b78c858418b704bce6a7d
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-17 12:48:31 +0200

    [Add]Confirmation token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_141545) do
+ActiveRecord::Schema.define(version: 2018_07_17_102413) do
 
   create_table "departments", force: :cascade do |t|
     t.string "department_name"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2018_07_16_141545) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
