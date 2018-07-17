7624be4397a5c82abaf13b862bd7e34b49c79644
Piotr <piotr.hacas@gmail.com>
2018-07-17 11:03:18 +0200

    [add] create comments migration and user_id to comment

--- db/migrate/20180717083913_create_roles.rb
+++ db/migrate/20180717083913_create_roles.rb
@@ -1,4 +1,4 @@
-class CreateRole < ActiveRecord::Migration[5.2]
+class CreateRoles < ActiveRecord::Migration[5.2]
   def change
     create_table :roles do |t|
 	    t.string :role, :default => 'user'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_083913) do
+ActiveRecord::Schema.define(version: 2018_07_17_090004) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "department_name"
