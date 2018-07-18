411d5949063f04c88e093732ce22df18d9ca912a
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-18 10:53:04 +0200

    [Add]User email regex validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+    it { should allow_value("email@addresse.foo").for(:email) }
+    it { should_not allow_value("foo").for(:email) }
   end
 
   describe 'relations' do
