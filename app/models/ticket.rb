c167b8dc2ef44aa51716bb38d5415d1880db2fc6
binarintern <kowal.bartek.10@gmail.com>
2018-07-19 11:15:35 +0200

    Style in views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,10 @@ class Ticket < ActiveRecord::Base
     
     belongs_to :user
 
-    validates :note, presence: true, length: { in: 30..500}
-    validates :title, presence: true, length: { in: 10..30}
+    validates :note, presence: true, length: { in: 20..500}
+    validates :title, presence: true, length: { in: 5..30}
+    validates :user_id, presence: true
+    validates :department, presence: true
 
     def fullticket
         " #{user_id} #{title} #{note} #{department} #{status} "
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-    let!(:ticket) {Ticket.create(title: 'test ticket', note: 'example ticket note with some words without sense', department: 'IT', status: 'open', user: '1')}
+    let!(:ticket) {Ticket.create(title: 'test ticket', note: 'example ticket note with some words without sense', department: 'IT', status: 'open', user_id: 1)}
     describe 'validations' do
         it { should validate_presence_of(:title) }
         it { should validate_presence_of(:note) }
-        it { should validate_presence_of(:user) }
+        it { should validate_presence_of(:user_id) }
         it { should validate_presence_of(:department) }
     end
 
     describe 'attributes' do
         it 'should have proper attributes' do
-            expect(subject.attributes).to include('title', 'note', 'status', 'user', 'department') 
+            expect(subject.attributes).to include('title', 'note', 'status', 'user_id', 'department') 
         end
     end
 
@@ -21,7 +21,7 @@ RSpec.describe Ticket, type: :model do
 
     describe '#fullticket' do
         it 'should have working #fullticket method' do
-            expect(ticket.fullpost).to eq('1 test ticket example ticket note with some words without sense IT open')
+            expect(ticket.fullticket).to eq('1 test ticket example ticket note with some words without sense IT open ')
         end
     end
 end
