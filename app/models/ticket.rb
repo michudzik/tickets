00e4425b6b292cb473324d72798eea51099b4229
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-19 13:32:25 +0200

    [Add]Relationship between models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,21 @@ class Ticket < ActiveRecord::Base
     
     belongs_to :user
     belongs_to :department
+    belongs_to :status
 
     validates :note, presence: true, length: { in: 30..500}
     validates :title, presence: true, length: { in: 10..30}
+
+    before_validation :default_status, on: :create
 
     def fullticket
         " #{user_id} #{title} #{note} #{department} #{status} "
     end
 
+
+    private
+
+      def default_status
+        self.status ||= Status.find_by(status: 'open')
+      end
 end
