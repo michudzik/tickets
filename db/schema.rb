50676a7111e2f48c7a915e9784ac07f02922b403
Michał Chudzik <MICHALCHUDZIK@LIVE.COM>
2018-07-23 15:19:53 +0200

    [Add] Index on role id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_105950) do
+ActiveRecord::Schema.define(version: 2018_07_23_121100) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2018_07_23_105950) do
     t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
