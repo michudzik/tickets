af7293b9d490847f8af14d41b2ac22f7eedee8c9
Michał Chudzik <MICHALCHUDZIK@LIVE.COM>
2018-07-23 12:13:09 +0200

    [Fix] Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  #skip_before_action :authenticate_user!, if: :devise_controller?
 
   protected
 
-    def ensure_admin
-      unless current_user.admin?
-        redirect_to root_url, alert: 'No access'
-      end
-    end
+  def ensure_admin
+      redirect_to(root_url, alert: 'No access') unless current_user.admin?
+  end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :password, :password_confirmation, :current_password])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :password, :password_confirmation, :current_password])
+  end
 
 end
