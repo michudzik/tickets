77b16cac11d13c08f98e3419fdd7dac2ea6a427b
Michał Chudzik <MICHALCHUDZIK@LIVE.COM>
2018-07-25 19:09:33 +0200

    [Add] Mock API keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,11 @@
 Recaptcha.configure do |config|
-  config.site_key   = Rails.application.credentials[Rails.env.to_sym][:RECAPTCHA_SITE_KEY]
-  config.secret_key = Rails.application.credentials[Rails.env.to_sym][:RECAPTCHA_SECRET_KEY]
+  if Rails.env.production? || Rails.env.development?
+    config.site_key   = Rails.application.credentials[Rails.env.to_sym][:RECAPTCHA_SITE_KEY]
+    config.secret_key = Rails.application.credentials[Rails.env.to_sym][:RECAPTCHA_SECRET_KEY]
+  else
+    config.site_key = 'somesecretsitekey'
+    config.secret_key = 'somesecretsecretkey'
+  end
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
