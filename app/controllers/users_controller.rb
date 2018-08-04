a2f8691afd9c26b23117b1ec3da3f329ec151dfe
Michał Chudzik <MICHALCHUDZIK@LIVE.COM>
2018-08-04 12:45:41 +0200

    [Refactor] User's index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,8 @@ class UsersController < ApplicationController
   def index
     @roles = Role.all.map { |role| [role.name.humanize.to_s, role.id] }
     @users = User.all
-
-    case params[:filter_param]
-    when 'locked'
-      @users = @users.locked
-    when 'unlocked'
-      @users = @users.unlocked
-    end
-
-    case params[:sorted_by]
-    when 'last_name_asc'
-      @users = @users.ordered_by_last_name_asc
-    when 'last_name_desc'
-      @users = @users.ordered_by_last_name_desc
-    when 'email_asc'
-      @users = @users.ordered_by_email_asc
-    when 'email_desc'
-      @users = @users.ordered_by_email_desc
-    end
-
+    @users = @users.filter_users(params[:filter_param]) if params[:filter_param]
+    @users = @users.sort_users(params[:sorted_by]) if params[:sorted_by]
     @users = @users.paginate(page: params[:page], per_page: params[:number])
   end
 
