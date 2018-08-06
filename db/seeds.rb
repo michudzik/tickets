604f24cd17ef66c7e7be06081a77b44ea9f2417b
Michał Chudzik <michalchudzik@live.com>
2018-08-06 16:55:18 +0200

    Refine seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ admin_role = Role.find_or_create_by({ name: 'admin' })
 
 admin = User.create(first_name: 'admin', last_name: 'admin', email: 'admin@admin.com', password: 'secret', confirmed_at: DateTime.now, role_id: admin_role.id)
 it_support = User.create(first_name: 'it', last_name: 'support', email: 'itsupport@example.com', password: 'secret', confirmed_at: DateTime.now, role_id: it_support_role.id)
-om_support = User.create(first_name: 'admin', last_name: 'admin', email: 'omsupport@example.com', password: 'secret', confirmed_at: DateTime.now, role_id: om_support_role.id)
-user = User.create(first_name: 'admin', last_name: 'admin', email: 'user@example.com', password: 'secret', confirmed_at: DateTime.now, role_id: user_role.id)
+om_support = User.create(first_name: 'om', last_name: 'support', email: 'omsupport@example.com', password: 'secret', confirmed_at: DateTime.now, role_id: om_support_role.id)
+user = User.create(first_name: 'user', last_name: 'user', email: 'user@example.com', password: 'secret', confirmed_at: DateTime.now, role_id: user_role.id)
 
 it_department = Department.find_or_create_by({ name: 'IT' })
 om_department = Department.find_or_create_by({ name: 'OM' })
@@ -24,7 +24,8 @@ status_closed = Status.find_or_create_by({ name: 'closed' })
 status_user_response = Status.find_or_create_by({ name: 'user_response' })
 status_support_response = Status.find_or_create_by({ name: 'support_response' })
 
-Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: admin.id, department_id: it_department.id, status_id: status_open.id)
-Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: it_support.id, department_id: om_department.id, status_id: status_user_response.id)
-Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: om_support.id, department_id: it_department.id, status_id: status_closed.id)
-Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: user.id, department_id: it_department.id, status_id: status_support_response.id)
+
+t1 = Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: admin.id, department_id: it_department.id, status_id: status_open.id)
+t2 = Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: it_support.id, department_id: om_department.id, status_id: status_user_response.id)
+t3 = Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: om_support.id, department_id: it_department.id, status_id: status_closed.id)
+t4 = Ticket.create(title: Faker::Name.name, note: Faker::Lorem.sentences(3).join(''), user_id: user.id, department_id: it_department.id, status_id: status_support_response.id)
